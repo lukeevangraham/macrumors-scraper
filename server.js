f5db557eda2473b5fc902f2928db932ae5c796d0
Cefalu Wave <luke.evan.graham@gmail.com>
2019-07-01 23:39:16 -0700

    setting up server

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 var express = require("express");
-
-var PORT = process.env.PORT || 3000;
+var mongoose = require("mongoose");
 
 var expressHandlebars = require("express-handlebars");
-var mongoose = require("mongoose");
+
+// Requiring the `mongoHeadlines` model for accessing the `mongoHeadline` collection
+var Example = require("./mongoHeadlines.js");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
 
 var app = express();
  
@@ -22,7 +28,7 @@ app.get('/', function (req, res) {
  
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
