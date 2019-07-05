70ae58294d1fa79e5c8f37bbc4e30336e4805688
Cefalu Wave <luke.evan.graham@gmail.com>
2019-07-05 01:05:53 -0700

    working on displaying comments

--- models/Article.js
+++ models/Article.js
@@ -49,9 +49,9 @@ var ArticleSchema = new Schema({
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
-  note: {
+  post: {
     type: Schema.Types.ObjectId,
-    ref: "Note"
+    ref: "Comment"
   }
 });
 
---
+++ public/app.js
@@ -0,0 +1,31 @@
+// When you click the postcomment button
+$(document).on("click", "#postcomment", function() {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
+
+    // console.log($("#bodyinput").val());
+  
+    // Run a POST request to change the comment, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        // Value taken from title input
+        // title: $("#bodyinput").val(),
+        // Value taken from note textarea
+        post: $("#bodyinput").val()
+      }
+    })
+      // With that done
+      .then(function(data) {
+        // Log the response
+        console.log(data);
+        // Empty the comments section
+        $("#bodyinput").empty();
+      });
+  
+    // Also, remove the values entered in the input and textarea for note entry
+    // $("#titleinput").val("");
+    $("#bodyinput").val("");
+  });
+  
