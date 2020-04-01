c09e7e270a392f44bd3e3e78e282e49b08a34c35
Cefalu Wave <luke.evan.graham@gmail.com>
2020-03-31 20:18:43 -0700

    working again after macrumors changed their code

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -6,25 +6,30 @@ var cheerio = require("cheerio");
 let scrape = function (cb) {
     console.log("scrape initiated!")
 
-    axios.get("https://www.macrumors.com/").then(function (response) {
+    axios.get("https://www.macrumors.com/").then(function(response) {
         let $ = cheerio.load(response.data);
+
+        // console.log("LOOK HERE: ", $)
+
+        // console.log("THIS ONE: ", response.data)
         
         let scrapeCount = 0
         
         // var result = {};
         let result = [];
         
-        $(".article").each(function (i, element) {
+        $(".js-article").each(function (i, element) {
             
-            let str = $(this).children(".content").children(".content_inner").text()
+            let str = $(this).children(".js-content").find(".js-contentInner").text()
             
             let newTrimmedString = str.split('.')[0] + ".";
             
             let dataToAdd = {
-                article : $(this).children("h2.title").children("a").text(),
-                link : $(this).children("h2.title").children("a").attr("href"),
+                article : $(this).find("h1").children("a").text(),
+                link : $(this).find("h1").children("a").attr("href"),
                 summary : newTrimmedString
             };
+            // console.log("HERE: ", dataToAdd)
 
             result.push(dataToAdd);
         })
