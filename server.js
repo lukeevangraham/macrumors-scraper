e378696e1a831b1a35d03f44db0ede981ea62a9c
Cefalu Wave <luke.evan.graham@gmail.com>
2020-04-08 22:01:08 -0700

    relative file pths

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require('body-parser')
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3500;
 
 var app = express();
 
